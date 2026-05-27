7c573af5099fe24c67fdc7c15396c009bad64658
AscendusEduTec <ascendusthiago@gmail.com>
2026-05-27 10:18:50 -0300

    adiciona entidade JPA Usuario

---
+++ backend/src/main/java/br/gov/mt/seplag/api/model/Usuario.java
@@ -0,0 +1,25 @@
+package br.gov.mt.seplag.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+
+}
