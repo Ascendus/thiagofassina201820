1dc2109c0981fbd8c047646982b1a1b94de258ce
AscendusEduTec <ascendusthiago@gmail.com>
2026-05-27 20:27:54 -0300

    feat: adiciona AlbumService

--- backend/src/main/java/br/gov/mt/seplag/api/dto/AlbumResponse.java
+++ backend/src/main/java/br/gov/mt/seplag/api/dto/AlbumResponse.java
@@ -3,9 +3,8 @@ package br.gov.mt.seplag.api.dto;
 import java.util.List;
 
 public record AlbumResponse(
-
         Long id,
         String titulo,
         List<String> imagensUrl
-
-) {}
+) {
+}
---
+++ backend/src/main/java/br/gov/mt/seplag/api/service/AlbumService.java
@@ -0,0 +1,49 @@
+package br.gov.mt.seplag.api.service;
+
+import br.gov.mt.seplag.api.dto.AlbumRequest;
+import br.gov.mt.seplag.api.dto.AlbumResponse;
+import br.gov.mt.seplag.api.model.Album;
+import br.gov.mt.seplag.api.model.Artista;
+import br.gov.mt.seplag.api.repository.AlbumRepository;
+import br.gov.mt.seplag.api.repository.ArtistaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AlbumService {
+
+    private final AlbumRepository albumRepository;
+    private final ArtistaRepository artistaRepository;
+
+    public AlbumService(AlbumRepository albumRepository, ArtistaRepository artistaRepository) {
+        this.albumRepository = albumRepository;
+        this.artistaRepository = artistaRepository;
+    }
+
+    public Page<AlbumResponse> listarPorArtista(Long artistaId, Pageable pageable) {
+        return albumRepository
+                .findByArtistasId(artistaId, pageable)
+                .map(this::toResponse);
+    }
+
+    public AlbumResponse criar(AlbumRequest request) {
+        Artista artista = artistaRepository.findById(request.artistaId()).orElseThrow(() -> new RuntimeException("Artista não encontrado"));
+        Album album = new Album();
+        album.setTitulo(request.titulo());
+        artista.getAlbuns().add(album);
+        artistaRepository.save(artista);
+        return toResponse(albumRepository.save(album));
+    }
+
+    private AlbumResponse toResponse(Album album) {
+        return new AlbumResponse(
+                album.getId(),
+                album.getTitulo(),
+                Collections.emptyList());
+    }
+
+
+}
