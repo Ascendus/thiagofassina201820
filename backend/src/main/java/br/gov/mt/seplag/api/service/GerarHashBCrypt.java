4a11b9d0eaefca47403038eb68aa83f7f65f2d95
AscendusEduTec <ascendusthiago@gmail.com>
2026-05-28 11:44:33 -0300

    hashbycript.

--- backend/src/main/java/br/gov/mt/seplag/api/service/GerarHashBCrypt.java
+++ backend/src/main/java/br/gov/mt/seplag/api/service/GerarHashBCrypt.java
@@ -2,7 +2,7 @@ package br.gov.mt.seplag.api.service;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class GerarHash {
+public class GerarHashBCrypt {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         System.out.println(encoder.encode("admin123"));
